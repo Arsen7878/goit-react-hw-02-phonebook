2352a659518e84031f45d93b7487a4c2aeb4a850
Arsen Temurian <arsentim2611@gmail.com>
2021-12-03 18:21:53 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import c from './ContactForm.module.css';
+import PropTypes from 'prop-types';
 
 class ContactForm extends Component {
   state = {
@@ -70,5 +71,9 @@ class ContactForm extends Component {
     );
   }
 }
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default ContactForm;
